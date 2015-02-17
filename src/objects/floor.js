bbe440cae591d8f19807d6bac47e01861b037b58
Brian Chirls <brian@chirls.com>
2015-02-17 10:54:43 -0500

    Make floor much bigger and circular

--- src/objects/floor.js
+++ src/objects/floor.js
@@ -8,12 +8,14 @@ module.exports = (function () {
 		var obj,
 			geometry;
 
-		geometry = new THREE.PlaneBufferGeometry(10, 10, options.widthSegments, options.heightSegments);
+		geometry = new THREE.CircleGeometry( options.radius || 100, options.segments || 16 );
 		geometry.applyMatrix( new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 
 		obj = new THREE.Mesh(
 			geometry,
-			materials.checkerboard()
+			materials.checkerboard({
+				repeat: 100
+			})
 		);
 		obj.name = 'floor';
 
