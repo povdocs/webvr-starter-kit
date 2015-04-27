502aba1f4c8c118bbffc1b23670a425f359c1075
Brian Chirls <brian@chirls.com>
2015-04-27 18:32:22 -0400

    Move urlRegex into its own file so it can be re-used

--- src/materials.js
+++ src/materials.js
@@ -9,52 +9,7 @@ module.exports = (function () {
 
 		// https://gist.github.com/dperini/729294
 		//urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i,
-		urlRegex = new RegExp(
-			'^' +
-
-			// protocol identifier
-			'(?:(?:(?:https?):)?//)' +
-
-			// user:pass authentication
-			'(?:\\S+(?::\\S*)?@)?' +
-			'(?:' +
-
-			// IP address exclusion
-			// private & local networks
-			'(' +
-			'(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
-			'(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
-			'(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
-
-			// IP address dotted notation octets
-			// excludes loopback network 0.0.0.0
-			// excludes reserved space >= 224.0.0.0
-			// excludes network & broacast addresses
-			// (first & last IP address of each class)
-			'(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
-			'(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
-			'(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
-			'|' +
-
-			// host name
-			'(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)' +
-
-			// domain name
-			'(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*' +
-
-			// TLD identifier
-			'(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))?' +
-			')' +
-
-			')' +
-
-			// port number
-			'(?::(\\d{2,5}))?' +
-
-			// resource path
-			'(?:/\\S*)?' +
-			'$', 'i'
-		),
+		urlRegex = require('./utils/urlregex'),
 
 		dataUri = /^data:image\/(?:png|jpe?g|gif);/,
 		assetPath = __ASSET_PATH__,
@@ -170,7 +125,7 @@ module.exports = (function () {
 			image = images[src];
 		} else {
 			image = document.createElement('img');
-			if (parse && (parse[1] && parse[1] !== window.location.host || parse[2] && parse[2] !== window.location.port)) {
+			if (parse && (parse[1] && parse[1] !== window.location.hostname || parse[2] && parse[2] !== window.location.port)) {
 				image.crossOrigin = 'anonymous';
 			}
 			image.src = src;
